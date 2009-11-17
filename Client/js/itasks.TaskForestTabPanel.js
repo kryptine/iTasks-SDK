4fd9a20bc3e49dd66058e159009bd3683d277730
Bas Lijnse <b.lijnse@cs.ru.nl>
2009-11-17 10:19:55 +0000

    Updated handler url
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@685 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/js/itasks.TaskForestTabPanel.js
+++ Client/js/itasks.TaskForestTabPanel.js
@@ -42,7 +42,7 @@ itasks.TaskForestTabPanel = Ext.extend( Ext.Panel, {
 					}],	
 			
 			loader: new Ext.tree.TreeLoader({
-						dataUrl: itasks.config.serverUrl + "/debug/tasktreeforest",
+						dataUrl: itasks.config.serverUrl + "/debug/taskforest",
 						baseParams: {_session: itasks.app.session},
 						requestMethod: "POST",
 						uiProviders: { col : Ext.ux.tree.ColumnNodeUI }
