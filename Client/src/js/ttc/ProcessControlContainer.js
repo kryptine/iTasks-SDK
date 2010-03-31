b22d956ba72e3daa261141c1c786aec689f6e57d
ecrombag <noreply@itasks.org>
2010-03-31 09:06:48 +0000

    Updated the look of process control panel (or waiting panel)
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@934 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/src/js/ttc/ProcessControlContainer.js
+++ Client/src/js/ttc/ProcessControlContainer.js
@@ -1,34 +1,47 @@
 Ext.ns('itasks.ttc');
 
 itasks.ttc.ProcessControlContainer = Ext.extend(Ext.Panel,{
-	initComponent: function() {	
-			
-		Ext.apply(this, {
-			cls: "worktab-content",
-			border: false,
-			hideBorders: true,
-			items: [{
-					html: "Waiting for <i>" +  this.properties.managerProps.subject + "</i>",
-					style: "margin: 0px 0px 20px 0px;"			
+	initComponent: function(){
+		
+		Ext.apply(this,
+		{ unstyled: true
+		, cls: 'ProcessControlContainer'	
+		, items: [
+			{ xtype: 'panel'
+			, cls: 'ProcessControlDescription task-description'
+			, unstyled: true
+			, html: 'Control process properties'
+			, width: 700
+			},
+			{ xtype: 'panel'
+			, unstyled: true
+			, layout: "form"
+			, defaultType: "staticfield"
+			, width: 700
+			, cls: 'ProcessControlPanel'
+			, items: [
+				{ html: "Waiting for <i>" +  this.properties.managerProps.subject + "</i>"
+				, style: "margin: 0px 0px 20px 0px;"
+				, unstyled: true			
+				, xtype: 'panel'
 				},{
 					xtype: "panel",
 					layout: "form",
 					defaultType: "staticfield",
+					unstyled: true,
 					items: [{
 						xtype: "itasks.userfield",
 						fieldLabel: "Assigned to",
 						value: this.properties.managerProps.worker,
 						listeners: {
-							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"user",nv);}
-									   , scope: this }
+							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"user",nv);}, scope: this }
 						}
 					},{
 						xtype: "itasks.priority",
 						fieldLabel: "Priority",
 						value: this.properties.managerProps.priority, 
 						listeners: {
-							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"priority",nv);}
-									   , scope: this }
+							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"priority",nv);}, scope: this }
 						}
 					},{
 						fieldLabel: "Progress",
@@ -46,22 +59,28 @@ itasks.ttc.ProcessControlContainer = Ext.extend(Ext.Panel,{
 						fieldLabel: "Last worked on",
 						format: itasks.util.formatStartDate,
 						value: this.properties.systemProps.latestEvent
-					}]
-				}]
-		});
-		itasks.ttc.ProcessControlContainer.superclass.initComponent.apply(this,arguments);
+					}
+				]}
+			]}
+		]});
+		
+		itasks.ttc.ProcessControlContainer.superclass.initComponent.apply(this,arguments);	
 	},
+	
 	update: function (data) {
 		this.properties = data.properties;
 
 		var p = data.properties;
 		var props = [p.managerProps.worker,p.managerProps.priority,p.workerProps.progress,p.systemProps.issuedAt,p.systemProps.firstEvent,p.systemProps.latestEvent];
 				
-		if(this.getComponent(0).body) this.getComponent(0).body.update("Waiting for <i>" + Ext.util.Format.htmlEncode(data.properties.managerProps.subject) + "</i>");
-		this.getComponent(1).items.each(function(cmt,i){ cmt.setValue(props[i]); });
+		if(this.getComponent(1).getComponent(0).body) this.getComponent(1).getComponent(0).body.update("Waiting for <i>" + Ext.util.Format.htmlEncode(data.properties.managerProps.subject) + "</i>");
+		this.getComponent(1).getComponent(1).items.each(function(cmt,i){ cmt.setValue(props[i]); });
 	}
 });
 
+Ext.ns('itasks.ttc.process');
+
+/*
 itasks.form.PriorityField = Ext.extend(itasks.form.InlineField, {
 	format: itasks.util.formatPriority,
 	field: {
@@ -72,6 +91,14 @@ itasks.form.PriorityField = Ext.extend(itasks.form.InlineField, {
 		triggerAction: "all",
 		forceSelection: true
 	}
+});*/
+
+itasks.form.PriorityField = Ext.extend(Ext.form.ComboBox,{
+	store: [["HighPriority","High"],["NormalPriority","Normal"],["LowPriority","Low"]],
+	triggerAction: 'all',
+	editable: false,
+	forceSelection: true,
+	format: itasks.util.formPriority
 });
 
 itasks.form.ProgressField = Ext.extend(itasks.form.InlineField, {
