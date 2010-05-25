98fe2cf20edacb9b3904a31590be83602b632a65
ecrombag <noreply@itasks.org>
2010-05-25 11:08:29 +0000

    bugfix: When a field was selected en then unfocussed, the error message could disappear.
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1017 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/src/js/tui/TUICommon.js
+++ Client/src/js/tui/TUICommon.js
@@ -1,6 +1,47 @@
 Ext.ns('itasks.tui.common');
 
+itasks.tui.common.markError = function(field,msg){
+	field.msgTarget = 'side';
+	
+	if(field.rendered && !field.preventMark){
+		msg = msg || field.errorText;
+		
+		var mt = field.getMessageHandler();
+		
+		if(mt && mt.markHint){
+			mt.markError(field, msg);
+		}else if(field.errorTarget){
+			var t = Ext.getDom(field.errorTarget);
+			if(t){
+				t.innerHTML = msg;
+				t.style.display = field.msgDisplay;
+			}
+		}
+	}
+	
+	field.activeError = msg;
+};
+
+itasks.tui.common.clearError = function(field){
+	if(field.rendered && !field.preventMark){
+		var mt = field.getMessageHandler();
+		if(mt && mt.clearError){
+			mt.clearError(field);
+		}else if(field.errorTarget){
+			var t = Ext.getDom(field.errorTarget);
+			if(t){
+				t.innerHTML = '';
+				t.style.display = 'none';
+			}		
+		}
+	}
+	
+	delete field.activeError;
+};
+
 itasks.tui.common.markHint = function(field,msg){
+	field.msgTarget = 'side';
+	
 	if(field.rendered && !field.preventMark){
 		msg = msg || field.hintText;
 		
@@ -37,7 +78,7 @@ itasks.tui.common.clearHint = function(field){
 	delete field.activeHint;
 };
 
-itasks.tui.common.alignHintIcon = function(field){
+itasks.tui.common.alignHintIcon = function(field){	
 	if(field.wrap){
 		field.hintIcon.alignTo(field.wrap, 'tl-tr', [2, 0]);
 	}else{
@@ -45,8 +86,49 @@ itasks.tui.common.alignHintIcon = function(field){
 	}
 };
 
+itasks.tui.common.alignErrIcon = function(field){
+	if(field.wrap){
+		field.errIcon.alignTo(field.wrap, 'tl-tr', [2, 0]);
+	}else{
+		field.errIcon.alignTo(field.el, 'tl-tr', [2, 0]);
+	}
+};
+
 //append the hint functions to the MessageTarget Framework
 Ext.apply(Ext.form.MessageTargets.side, {
+	markError : function(field, msg){
+		if(!field.errIcon){
+			var elp = field.getErrorCt();
+			
+			if(!elp){
+				field.el.dom.title = msg;
+				return;
+			}
+			
+			field.errIcon = elp.createChild({cls: 'x-form-invalid-icon'});
+			if(field.ownerCt){
+				field.ownerCt.on('afterlayout', function(){ itasks.tui.common.alignErrIcon(this)}, field);
+				field.ownerCt.on('expand', function(){ itasks.tui.common.alignErrIcon(this)}, field);
+			}
+			field.on('resize', function(){ itasks.tui.common.alignErrIcon(this)}, field);
+			field.on('destroy', function(){ Ext.destroy(this.errIcon); },field);
+		}
+		
+		itasks.tui.common.alignErrIcon(field);
+		field.errIcon.dom.qtip = msg;
+		field.errIcon.dom.qclass = 'x-form-invalid-tip';
+		field.errIcon.show();	
+	},
+	
+	clearError : function(field){
+		if(field.errIcon){
+			field.errIcon.dom.qtip = '';
+			field.errIcon.hide();
+		}else{
+			field.el.dom.title = '';
+		}
+	},
+	
 	markHint : function(field, msg){
 		if(!this.activeError && !field.hintIcon){
 			var elp = field.getErrorCt();
@@ -58,10 +140,10 @@ Ext.apply(Ext.form.MessageTargets.side, {
 			
 			field.hintIcon = elp.createChild({cls: 'x-form-hint-icon'});
 			if(field.ownerCt){
-				field.ownerCt.on('afterlayout', itasks.tui.common.alignHintIcon(field), field);
-				field.ownerCt.on('expand', itasks.tui.common.alignHintIcon(field), field);
+				field.ownerCt.on('afterlayout', function() {itasks.tui.common.alignHintIcon(this)}, field);
+				field.ownerCt.on('expand', function() {itasks.tui.common.alignHintIcon(this)}, field);
 			}
-			field.on('resize', itasks.tui.common.alignHintIcon(field), field);
+			field.on('resize', function() {itasks.tui.common.alignHintIcon(this)}, field);
 			field.on('destroy', function(){ Ext.destroy(this.hintIcon); }, field);
 		}
 		
@@ -71,7 +153,7 @@ Ext.apply(Ext.form.MessageTargets.side, {
 		field.hintIcon.show();		
 	},
 	
-	clearHint: function(field,msg){
+	clearHint: function(field){
 		if(field.hintIcon){
 			field.hintIcon.dom.qtip = '';
 			field.hintIcon.hide();
