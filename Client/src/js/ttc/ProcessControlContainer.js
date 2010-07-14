eac75cf1fafe01f78798931617b73fd571b87cfa
ecrombag <noreply@itasks.org>
2010-07-14 13:55:15 +0000

    Fixed the process control panel. It was referring to the 'old' ...props instead of ...properties.
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1093 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/src/js/ttc/ProcessControlContainer.js
+++ Client/src/js/ttc/ProcessControlContainer.js
@@ -21,7 +21,7 @@ itasks.ttc.ProcessControlContainer = Ext.extend(Ext.Panel,{
 			, cls: 'ProcessControlPanel'
 			, width: 720
 			, items: [
-				{ html: "Waiting for <i>" +  this.properties.managerProps.subject + "</i>"
+				{ html: "Waiting for <i>" +  this.properties.managerProperties.subject + "</i>"
 				, style: "margin: 0px 0px 20px 0px;"
 				, unstyled: true			
 				, xtype: 'panel'
@@ -33,33 +33,33 @@ itasks.ttc.ProcessControlContainer = Ext.extend(Ext.Panel,{
 					items: [{
 						xtype: "itasks.userfield",
 						fieldLabel: "Assigned to",
-						value: this.properties.managerProps.worker,
+						value: this.properties.managerProperties.worker,
 						listeners: {
-							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"user",nv);}, scope: this }
+							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProperties.taskId,"user",nv);}, scope: this }
 						}
 					},{
 						xtype: "itasks.priority",
 						fieldLabel: "Priority",
-						value: this.properties.managerProps.priority, 
+						value: this.properties.managerProperties.priority, 
 						listeners: {
-							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProps.processId,"priority",nv);}, scope: this }
+							"change" : { fn: function(ov,nv) {this.findParentByType(itasks.WorkPanel).sendPropertyEvent(this.properties.systemProperties.taskId,"priority",nv);}, scope: this }
 						}
 					},{
 						fieldLabel: "Progress",
 						format: itasks.util.formatProgress,
-						value: this.properties.workerProps.progress
+						value: this.properties.workerProperties.progress
 					},{
 						fieldLabel: "Issued at",
 						format: itasks.util.formatDate,
-						value: this.properties.systemProps.issuedAt
+						value: this.properties.systemProperties.issuedAt
 					},{
 						fieldLabel: "First worked on",
 						format: itasks.util.formatStartDate,
-						value: this.properties.systemProps.firstEvent
+						value: this.properties.systemProperties.firstEvent
 					},{
 						fieldLabel: "Last worked on",
 						format: itasks.util.formatStartDate,
-						value: this.properties.systemProps.latestEvent
+						value: this.properties.systemProperties.latestEvent
 					}
 				]}
 			]}
@@ -72,9 +72,9 @@ itasks.ttc.ProcessControlContainer = Ext.extend(Ext.Panel,{
 		this.properties = data.properties;
 
 		var p = data.properties;
-		var props = [p.managerProps.worker,p.managerProps.priority,p.workerProps.progress,p.systemProps.issuedAt,p.systemProps.firstEvent,p.systemProps.latestEvent];
+		var props = [p.managerProperties.worker,p.managerProperties.priority,p.workerProperties.progress,p.systemProperties.issuedAt,p.systemProperties.firstEvent,p.systemProperties.latestEvent];
 				
-		if(this.getComponent(1).getComponent(0).body) this.getComponent(1).getComponent(0).body.update("Waiting for <i>" + Ext.util.Format.htmlEncode(data.properties.managerProps.subject) + "</i>");
+		if(this.getComponent(1).getComponent(0).body) this.getComponent(1).getComponent(0).body.update("Waiting for <i>" + Ext.util.Format.htmlEncode(p.managerProperties.subject) + "</i>");
 		this.getComponent(1).getComponent(1).items.each(function(cmt,i){ cmt.setValue(props[i]); });
 	}
 });
