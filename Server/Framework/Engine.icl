kryptine/iTasks-SDK

commit 2ff593268d12f188d4aac1e42c8662e98eb88e94

Using new directory module from Clean-platform
git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1424 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

Server/Framework/Engine.icl
@@ -1,15 +1,14 @@
 implementation module Engine
 
-import StdMisc, StdArray, StdList, StdChar, StdFile, StdBool
+import StdMisc, StdArray, StdList, StdTuple, StdChar, StdFile, StdBool
 
 from StdFunc import o
 
 from StdLibMisc import qualified ::Date{..}, ::Time{..}
-from Directory import qualified pd_StringToPath, createDirectory, getFileInfo, ::DirError(..), ::FileInfo(..), ::PI_FileInfo(..), instance == DirError
 
 import	Store, UserDB, ProcessDB, SessionDB
 import	Util, HtmlUtil
-import	CommandLine, File, FilePath, HTTP, Text, MIME, UrlEncoding
+import	CommandLine, File, FilePath, Directory, HTTP, Text, MIME, UrlEncoding
 
 import	TuningCombinators
 import	Setup
@@ -152,32 +151,34 @@ handleStopRequest req world = ({newHTTPResponse & rsp_headers = fromList [("X-Se
 initTSt :: !HTTPRequest !Config ![Workflow] !*World -> *TSt
 initTSt request config flows world
 	# (appName,world) 			= determineAppName world
-	# (pathstr,world)			= determineAppPath world
-	# ((ok, path),world)		= 'Directory'.pd_StringToPath (pathstr) world
-	| not ok					= abort "Cannot find the executable."
-	# ((err,info),world)		= 'Directory'.getFileInfo path world
-	| err <> 'Directory'.NoDirError			= abort "Cannot get executable info."
-	# (date,time)				= info.'Directory'.pi_fileInfo.'Directory'.lastModified
-	# datestr					= (toString date.'StdLibMisc'.Date.'StdLibMisc'.year)+++"."+++
-								   (padZero date.'StdLibMisc'.Date.'StdLibMisc'.month)+++"."+++
-								   (padZero date.'StdLibMisc'.Date.'StdLibMisc'.day)+++"-"+++
-								   (padZero time.'StdLibMisc'.Time.'StdLibMisc'.hours)+++"."+++
-								   (padZero time.'StdLibMisc'.Time.'StdLibMisc'.minutes)+++"."+++
-								   (padZero time.'StdLibMisc'.Time.'StdLibMisc'.seconds
+	# (appPath,world)			= determineAppPath world
+	# appPath					= takeDirectory appPath
+	# (res,world)				= getFileInfo appPath world
+	| isError res				= abort "Cannot get executable info."
+	# tm						= (fromOk res).lastModifiedTime
+	# datestr					= (toString tm.Tm.year)+++"."+++
+								   (padZero tm.Tm.mon)+++"."+++
+								   (padZero tm.Tm.mday)+++"-"+++
+								   (padZero tm.Tm.hour)+++"."+++
+								   (padZero tm.Tm.min)+++"."+++
+								   (padZero tm.Tm.sec
 								  )
-	# ((_,datapath),world)		= 'Directory'.pd_StringToPath appName world
-	# (err,world)				= 'Directory'.createDirectory datapath world
-	| err <> 'Directory'.NoDirError && err <> 'Directory'.AlreadyExists
-		= abort "Cannot create data directory"
-	# storePath					= appName </> datestr
+	# world						= ensureDir "data" (appPath </> appName) world
 	# tmpPath					= appName </> "tmp-" +++ datestr
-	# ((_,piTmpPath),world)		= 'Directory'.pd_StringToPath tmpPath world
-	# (err,world)				= 'Directory'.createDirectory piTmpPath world
-	| err <> 'Directory'.NoDirError && err <> 'Directory'.AlreadyExists
-		= abort "Cannot create tmp directory"
+	# world						= ensureDir "tmp" tmpPath world
+	# storePath					= appName </> datestr
 	= mkTSt appName config request flows (createStore storePath) tmpPath world
 where 
+	padZero :: !Int -> String
 	padZero number = (if (number < 10) "0" "") +++ toString number
+
+	ensureDir :: !String !FilePath *World -> *World
+	ensureDir name path world
+	# (exists, world) = fileExists path world
+	| exists = world
+	# (res, world) = createDirectory path world
+	| isError res = abort ("Cannot create " +++ name +++ " directory" +++ path +++ " : "  +++ snd (fromError res))
+	= world
 
 finalizeTSt :: !*TSt -> *World
 finalizeTSt tst=:{TSt|iworld={IWorld|world}} = world

Server/API/Core/BasicTasks/RPCTasks.icl
@@ -5,8 +5,7 @@ import StdInt
 import StdFile
 import StdTuple
 
-from Directory import ::Path, getCurrentDirectory, pathToPD_String //TODO: Replace by Clean-platform System.Directory
-
+import Directory
 import File
 import FilePath
 import JSON
@@ -68,9 +67,8 @@ where
 
 getITasksPath :: *World -> (String, *World)
 getITasksPath world
-# (path, world) = getCurrentDirectory world
-# (currentPath, world) = pathToPD_String path world	
-= (takeDirectory currentPath, world)			
+# (res, world) = getCurrentDirectory world
+= (takeDirectory (fromOk res), world)			
 
 curlError :: Int -> String
 curlError exitCode = 

Server/Framework/Store.icl
@@ -1,7 +1,7 @@
 implementation module Store
 
-import StdString, StdArray, StdChar, StdClass, StdInt, StdFile, StdList, StdMisc
-import Directory, Maybe, Map, Text, JSON, Functor, FilePath
+import StdString, StdArray, StdChar, StdClass, StdInt, StdFile, StdList, StdTuple, StdMisc
+import File, Directory, OSError, Maybe, Map, Text, JSON, Functor, FilePath
 from Time import :: Timestamp(..), instance < Timestamp, instance toInt Timestamp
 from Types import :: IWorld{store,world,timestamp}, :: Config
 import dynamic_string //Static dynamic serialization
@@ -183,23 +183,18 @@ deleteValues` delKey filterFuncCache filterFuncDisk iworld=:{store=store=:{locat
 	= iworld
 where
 	deleteFromDisk world
-		# ((ok,dir),world)			= pd_StringToPath location world
-		| not ok					= abort ("Cannot create path to " +++ location)
-			# ((err,files),world)	= getDirectoryContents dir world
-			| err <> NoDirError		= abort ("Cannot read store directory " +++ location)
-			= unlink dir files world
+		# (res, world) = readDirectory location world
+		| isError res = abort ("Cannot read store directory " +++ location +++ ": " +++ snd (fromError res))
+		= unlink location (fromOk res) world
 		where
 			unlink _ [] world
 				= world
 			unlink dir [f:fs] world
-				| filterFuncDisk delKey f.fileName
-					# (err,world) = fremove (pathDown dir f.fileName) world 
+				| filterFuncDisk delKey f
+					# (err,world) = deleteFile (dir </> f) world 
 					= unlink dir fs world
 				| otherwise
 					= unlink dir fs world
-
-			pathDown (RelativePath steps) step = RelativePath (steps ++ [PathDown step]) 
-			pathDown (AbsolutePath dn steps) step = AbsolutePath dn (steps ++ [PathDown step])
 
 copyValues :: !String !String !*IWorld -> *IWorld
 copyValues fromprefix toprefix iworld=:{store=store=:{location}}
@@ -212,17 +207,15 @@ where
 	newKey key	= toprefix +++ (key % (size fromprefix, size key))
 
 	copyOnDisk fromprefix toprefix location world
-		# ((ok,dir),world)		= pd_StringToPath location world
-		| not ok				= abort ("Cannot create path to " +++ location)
- 		# ((err,files),world)	= getDirectoryContents dir world
- 		| err <> NoDirError		= abort ("Cannot read store directory " +++ location)
- 		= copy fromprefix toprefix files world
+ 		# (res,world)	= readDirectory location world
+ 		| isError res	= abort ("Cannot read store directory " +++ location +++ ": " +++ snd (fromError res))
+ 		= copy fromprefix toprefix (fromOk res) world
 
 	copy fromprefix toprefix [] world = world
 	copy fromprefix toprefix [f:fs] world
-		| startsWith fromprefix f.fileName
-			# sfile	= location </> f.fileName
-			# dfile = location </> toprefix +++ (f.fileName % (size fromprefix, size f.fileName))
+		| startsWith fromprefix f
+			# sfile	= location </> f
+			# dfile = location </> toprefix +++ (f % (size fromprefix, size f))
 			# world	= fcopy sfile dfile world
 			= copy fromprefix toprefix fs world
 		| otherwise
@@ -253,13 +246,12 @@ where
 flushCache :: !*IWorld -> *IWorld
 flushCache iworld=:{world,store=store=:{cache,location}}
 	//Check if the location exists and create it otherwise
-	# ((ok,dir),world)	= pd_StringToPath location world
-	| not ok			= abort ("Cannot create storepath: " +++ location)
-	#(err,world)		= case getFileInfo dir world of
-							((DoesntExist,fileinfo),world)	= createDirectory dir world
-							(_,world)						= (NoDirError,world)
-	# ok				= case err of NoDirError = True; _ = False
-	| not ok			= abort ("Cannot create store: " +++ location)
+	# (exists,world)	= fileExists location world
+	# world				= if exists world
+							( case createDirectory location world of
+								(Ok Void, world) = world
+								(Error e, world) = abort ("Cannot create store: " +++ location +++ ": " +++ snd e)
+							)
 	//Write the states to disk
 	# (list, world) = flush (toList cache) world 
 	= {iworld & world = world, store = {store & cache = fromList list}}

Server/Framework/TSt.icl
@@ -1,13 +1,12 @@
 implementation module TSt
 
-import StdList, StdTuple, StdBool, StdMisc, Maybe, StdFile, File
+import StdList, StdTuple, StdBool, StdMisc, Maybe, StdFile, File, Directory
 import HTTP, Util, Text
 import ProcessDB, SessionDB, ChangeDB, DocumentDB, UserDB, TaskTree
 import GenEq, GenVisualize, GenUpdate, Store, Config, dynamic_string
 import CoreCombinators, InteractionTasks
 from StdFunc	import id, const, o, seq
 from JSON		import JSONDecode, fromJSON
-from Directory	import :: Path, pd_StringToPath, getDirectoryContents, :: DirError, :: DirEntry{fileName}
 
 ITERATION_THRESHOLD :== 10 // maximal number of allowed iterations during calculation of task tree
 
@@ -864,14 +863,14 @@ flushStore tst = appIWorldTSt flushCache tst
 
 deleteTmpFiles :: !id !*IWorld -> *IWorld | iTaskId id
 deleteTmpFiles taskId iworld=:{world,tmpDirectory}
-	# ((_,piPath),world)	= pd_StringToPath tmpDirectory world
-	# ((_,entries),world)	= getDirectoryContents piPath world
-	# world					= seq (map checkEntry entries) world
+	# (res, world)			= readDirectory tmpDirectory world
+	| isError res			= {iworld & world = world} //Ignore error
+	# world					= seq (map checkEntry (fromOk res)) world
 	= {iworld & world = world}
 where
 	prefix = iTaskId taskId ""
 
-	checkEntry {fileName} world
+	checkEntry fileName world
 		| startsWith prefix fileName
 			= snd (deleteFile (tmpDirectory </> fileName) world)
 		| otherwise

Server/Gin/GinConfig.icl
@@ -4,10 +4,10 @@ import Maybe
 import StdFile
 from StdFunc import iter
 
-from Directory import ::DiskName, ::Path(..), ::PathStep(..), getCurrentDirectory, pathToPD_String
-import Error
+import Directory
 from File import qualified fileExists, readFile, writeFile
 from FilePath import takeDirectory, </>
+import OSError
 
 import CommandLine
 from Engine import determineAppName
@@ -41,13 +41,13 @@ where
 
 	getITasksPath :: *World -> (String, *World)
 	getITasksPath world
-	# (currentPath, world) = currentDirectory world
-	= (iter 2 takeDirectory currentPath, world)
+	# (res, world) = getCurrentDirectory world
+	= (iter 2 takeDirectory (fromOk res), world)
 
 	getTempPath :: *World -> (String, *World)
 	getTempPath world
-	# (currentPath, world) = currentDirectory world
-	= (currentPath </> "Temp", world)
+	# (res, world) = getCurrentDirectory world
+	= (fromOk res </> "Temp", world)
 
 ginLoadConfig :: !*World -> (!Maybe GinConfig, !*World)
 ginLoadConfig world
@@ -85,8 +85,3 @@ ginCheckConfig config world
 # (ok, world) = 'File'.fileExists config.tempPath world
 | not ok = (Just "Temp path incorrect", world)
 = (Nothing, world)
-
-currentDirectory :: !*World -> (String, *World)
-currentDirectory world
-	# (path, world) = getCurrentDirectory world
-	= pathToPD_String path world	

Server/iTasks.env
@@ -21,7 +21,6 @@ Environments
 			Path:	{Application}\iTasks-SDK\Server\lib\CleanIDE\Util
 			Path:	{Application}\iTasks-SDK\Server\lib\CleanIDE\Win
 			Path:	{Application}\iTasks-SDK\Server\lib\Http
-			Path:	{Application}\Libraries\Directory
 			Path:	{Application}\iTasks-SDK\Server\lib\Platform\OS-Independent
 			Path:	{Application}\iTasks-SDK\Server\lib\Platform\OS-Independent\Data
 			Path:	{Application}\iTasks-SDK\Server\lib\Platform\OS-Independent\Database

Server/lib/CleanIDE/Win/PmCleanSystem.icl
@@ -1,10 +1,11 @@
 implementation module PmCleanSystem
 
 import StdEnv
-import Directory
-
-import OSError
+import File
 import FilePath
+import Directory
+import OSError
+
 from Maybe import qualified ::Maybe(..)
 from Process import qualified callProcess
 
@@ -286,21 +287,21 @@ Link linker` winfun path
 		optionspathname library_file_names object_file_names static_libraries static gen_relocs gen_linkmap
 		link_resources resource_path gen_dll dll_syms startupdir dynlstr _ use_64_bit_processor ps
 	# tooltempdir = maketempdir startupdir
-	# (ok,linker,linkerdir)				= mangleLinker linker` startupdir
+	# (ok,linker,linkerdir)		= mangleLinker linker` startupdir
 	| not ok
 		# ps					= winfun [linker] ps
 		= (ps,False)
 	# flags						= ApplicationOptionsToFlags applicationOptions
 	# optdirpath				= RemoveFilename optionspathname
-	# ((ok,pd_optdirpath),ps)	= accFiles (pd_StringToPath optdirpath) ps
-	| not ok
-		= (winfun ["Linker error: Unable to understand path: "+++optdirpath] ps,False)
-	# ((err,_),ps)				= accFiles (getDirectoryContents pd_optdirpath) ps
-	# (err,ps)					= case err of
-		DoesntExist		-> accFiles (createDirectory pd_optdirpath) ps
-		err  			-> (err,ps)
-	| err <> NoDirError
-		= (winfun ["Linker error: Unable to access or create: "+++optdirpath] ps,False)
+	# world						= ps.LogEnv.world
+	# (exists, world)			= fileExists optdirpath world
+	# (err, world)				= if exists (False, world)
+									( case createDirectory optdirpath world of
+										(Ok Void, world) = (False,world)
+										(Error _, world) = (True, world)
+									)
+	# ps = { ps & world = world }
+	| err = (winfun ["Linker error: Unable to access or create: "+++optdirpath] ps,False)
 	# (options_file_ok,ps)		= accFiles (write_options_file optionspathname flags hs ss initial_heap_size heap_size_multiple minheap use_64_bit_processor) ps
 	| not options_file_ok
 		= (winfun ["Linker error: Could not write the options object file: "+++optionspathname] ps,False)

commit 9411312cf52e7f63ca9428594fed1085181db190

Fixed bug in loading sessions introduced in r1424
git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1429 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

Server/Framework/Engine.icl
@@ -150,7 +150,7 @@ initTSt :: !HTTPRequest !Config ![Workflow] !*World -> *TSt
 initTSt request config flows world
 	# (appName,world) 			= determineAppName world
 	# (appPath,world)			= determineAppPath world
-	# appPath					= takeDirectory appPath
+	# appDir					= takeDirectory appPath
 	# (res,world)				= getFileInfo appPath world
 	| isError res				= abort "Cannot get executable info."
 	# tm						= (fromOk res).lastModifiedTime
@@ -161,8 +161,8 @@ initTSt request config flows world
 								   (padZero tm.Tm.min)+++"."+++
 								   (padZero tm.Tm.sec
 								  )
-	# world						= ensureDir "data" (appPath </> appName) world
-	# tmpPath					= appName </> "tmp-" +++ datestr
+	# world						= ensureDir "data" (appDir </> appName) world
+	# tmpPath					= appDir </> appName </> "tmp-" +++ datestr
 	# world						= ensureDir "tmp" tmpPath world
 	# storePath					= appName </> datestr
 	= mkTSt appName config request flows (createStore storePath) tmpPath world