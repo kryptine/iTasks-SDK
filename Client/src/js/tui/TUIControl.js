737350c1fbbfcbb0b8c3f998b400f13663d5b7b6
Steffen Michels <s.michels@science.ru.nl>
2011-05-25 14:48:08 +0000

    if controls are changed but don't loose focus, they nevertheless send an event after some interval
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1617 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/src/js/tui/TUIControl.js
+++ Client/src/js/tui/TUIControl.js
@@ -6,7 +6,7 @@ itasks.tui.extendControl = function(extSuper,overrides) {
 
 itasks.tui.control = Ext.apply(itasks.util.clone(itasks.tui.base),{
 	allowBlank:	true,
-
+	enableKeyEvents: true,
 	initComponent: function() {
 		if (!this.margins) {
 			this.margins =	{ left		: 3
@@ -16,7 +16,14 @@ itasks.tui.control = Ext.apply(itasks.util.clone(itasks.tui.base),{
 							};
 		}
 	
-		this.listeners = this.listeners || {change : {fn: this.onChange, scope: this}};
+		if (!Ext.isDefined(this.listeners)) {
+			this.listeners = {change : {fn: this.onChange, scope: this}};
+			
+			if (Ext.isNumber(itasks.config.eventTimerInterval)) {
+				this.listeners.keypress = {fn: this.onKeypress, scope: this};
+				this.delayedSendEvent = new Ext.util.DelayedTask(this.onChange,this);
+			}
+		}
 		
 		itasks.tui.base.initComponent.apply(this,arguments);
 
@@ -24,9 +31,20 @@ itasks.tui.control = Ext.apply(itasks.util.clone(itasks.tui.base),{
 		this.enableBubble('tuichange');
 	},
 	onChange: function() {
+		if (this.delayedSendEvent) {
+			this.eventTimerActive = false;
+			this.delayedSendEvent.cancel();
+		}
+		
 		if(this.isValid()) {
 			var v = this.getValue();
 			this.fireEvent('tuichange',this.taskId,this.name,v === "" ? null : v);
+		}
+	},
+	onKeypress: function() {
+		if (!this.eventTimerActive) {
+			this.eventTimerActive = true;
+			this.delayedSendEvent.delay(itasks.config.eventTimerInterval);
 		}
 	}
 });
