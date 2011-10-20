87130f4b89dcaab7c6d31fc221d68fd4790bd8b2
Bas Lijnse <b.lijnse@cs.ru.nl>
2011-10-20 09:46:14 +0000

    Improved visualization of Note datatype. Not entirely correct though :(
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1857 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/src/app/component/edit/Note.js
+++ Client/src/app/component/edit/Note.js
@@ -1,5 +1,12 @@
 Ext.define('itasks.component.edit.Note',{
 	alias: 'widget.itasks_edit_note',
 	extend: 'Ext.form.field.TextArea',
-	mixins: ['itasks.mixin.Editable']
+	mixins: ['itasks.mixin.Editable'],
+	initComponent: function() {
+		if(!this.width && !this.hflex) {
+			this.hflex = 1;
+			this.minWidth = 500;
+		}
+		this.callParent(arguments);
+	}
 });
--- Client/src/app/component/show/Note.js
+++ Client/src/app/component/show/Note.js
@@ -1,9 +1,12 @@
 Ext.define('itasks.component.show.Note',{
 	extend: 'Ext.Component',
 	alias: 'widget.itasks_show_note',
-	minWidth: 500,
-	minHeight: 100,
 	initComponent: function() {
-		this.html = this.value;
+		if(!this.width && !this.hflex) {
+			this.hflex = 1;
+			this.minWidth = 500;
+		}
+		this.html = Ext.String.htmlEncode(this.value).replace("\n","<br>","g");
+		
 	}
 });
