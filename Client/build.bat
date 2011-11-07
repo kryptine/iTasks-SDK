d4f1b35b2ae138432c12ed22e7e7ad72e49c08fc
Bas Lijnse <b.lijnse@cs.ru.nl>
2011-11-07 10:37:44 +0000

    updated build.bat, because it is currently broken not used, but it might be required again in the future.
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@1898 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/build.bat
+++ Client/build.bat
@@ -1,3 +1,4 @@
-java -jar ../Tools/JSBuilder2/JSBuilder2.jar --projectFile .\build.jsb2 --homeDir .
+:: java -jar ../Tools/JSBuilder2/JSBuilder2.jar --projectFile .\build.jsb2 --homeDir .
 @echo off
+echo The optimized client build is currently disabled. Please use the 'Client' folder as client path instead of 'Client/build'
 pause
