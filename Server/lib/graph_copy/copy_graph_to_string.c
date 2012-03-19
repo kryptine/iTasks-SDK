daba2e75bc26745892fd203ab2859dc1e083c928
Bas Lijnse <b.lijnse@cs.ru.nl>
2012-03-19 15:25:41 +0000

    Updated graph_copy c files
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@2026 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Server/lib/graph_copy/copy_graph_to_string.c
+++ Server/lib/graph_copy/copy_graph_to_string.c
@@ -4,11 +4,21 @@
 #ifdef _WIN64
 # define Int __int64
 # define INT_descriptor dINT
+# define ARCH_64 1
 #else
-# define Int int
-# define INT_descriptor INT
-# define __STRING__ _STRING__
-# define __ARRAY__ _ARRAY__
+# if defined (MACH_O64) || defined (LINUX64)
+#  define Int long long
+#  define INT_descriptor dINT
+#  define ARCH_64 1
+# else
+#  define Int int
+#  define INT_descriptor INT
+#  define ARCH_64 0
+# endif
+# if !(defined (LINUX32) || defined (LINUX64))
+#  define __STRING__ _STRING__
+#  define __ARRAY__ _ARRAY__
+# endif
 #endif
 
 extern void *INT_descriptor,*CHAR,*BOOL,*REAL,*__STRING__,*__ARRAY__;
@@ -72,7 +82,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 					arity=((unsigned short *)desc)[-1];
 					if (arity==0){
 						if (desc==(Int)&INT_descriptor+2 || desc==(Int)&CHAR+2 || desc==(Int)&BOOL+2
-#ifdef _WIN64
+#if ARCH_64
 							|| desc==(Int)&REAL+2
 #endif
 						){
@@ -81,7 +91,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 
 							*heap_p++=node_p[1];
 							break;
-#ifndef _WIN64
+#if ! ARCH_64
 						} else if (desc==(Int)&REAL+2){
 							if (heap_p+2>(Int*)stack_begin)
 								return NULL;
@@ -96,7 +106,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 							
 							length=node_p[1];
 							node_p+=2;
-#ifdef _WIN64
+#if ARCH_64
 							n_words=(length+7)>>3;							
 #else
 							n_words=(length+3)>>2;
@@ -135,7 +145,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 									stack_p[array_size]=(Int*)node_p[array_size];						
 								break;
 							} else if (elem_desc==(Int)&INT_descriptor+2
-#ifdef _WIN64
+#if ARCH_64
 								|| elem_desc==(Int)&REAL+2
 #endif
 							){
@@ -145,7 +155,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 								copy (heap_p,node_p,array_size);	
 								heap_p+=array_size;
 								break;
-#ifndef _WIN64
+#if ! ARCH_64
 							} else if (elem_desc==(Int)&REAL+2){
 								array_size<<=1;
 							
@@ -157,7 +167,7 @@ Int *copy_graph_to_string (Int *node_p,void *begin_free_heap,void *end_free_heap
 								break;
 #endif
 							} else if (elem_desc==(Int)&BOOL+2){
-#ifdef _WIN64
+#if ARCH_64
 								array_size=(array_size+7)>>3;
 #else
 								array_size=(array_size+3)>>2;
--- Server/lib/graph_copy/copy_string_to_graph.c
+++ Server/lib/graph_copy/copy_string_to_graph.c
@@ -4,11 +4,21 @@
 #ifdef _WIN64
 # define Int __int64
 # define INT_descriptor dINT
+# define ARCH_64 1
 #else
-# define Int int
-# define INT_descriptor INT
-# define __STRING__ _STRING__
-# define __ARRAY__ _ARRAY__
+# if defined (MACH_O64) || defined (LINUX64)
+#  define Int long long
+#  define INT_descriptor dINT
+#  define ARCH_64 1
+# else
+#  define Int int
+#  define INT_descriptor INT
+#  define ARCH_64 0
+# endif
+# if !(defined (LINUX32) || defined (LINUX64))
+#  define __STRING__ _STRING__
+#  define __ARRAY__ _ARRAY__
+# endif
 #endif
 
 extern void *INT_descriptor,*CHAR,*BOOL,*REAL,*__STRING__,*__ARRAY__;
@@ -92,7 +102,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 							string_p+=2;
 							break;
 						} else if (desc==(Int)&BOOL+2
-#ifdef _WIN64
+#if ARCH_64
 							|| desc==(Int)&REAL+2
 #endif
 						){
@@ -105,7 +115,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 							string_p+=2;
 							heap_p+=2;
 							break;
-#ifndef _WIN64
+#if ! ARCH_64
 						} else if (desc==(Int)&REAL+2){
 							n_free_words-=2;
 							if (n_free_words<0){
@@ -124,7 +134,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 							
 							length=string_p[1];
 							string_p+=2;
-#ifdef _WIN64
+#if ARCH_64
 							n_words=(length+7)>>3;
 #else
 							n_words=(length+3)>>2;
@@ -182,7 +192,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 								heap_p+=array_size;
 								break;
 							} else if (elem_desc==(Int)&INT_descriptor+2
-#ifdef _WIN64
+#if ARCH_64
 								|| elem_desc==(Int)&REAL+2
 #endif
 							){
@@ -196,7 +206,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 								string_p+=array_size;
 								heap_p+=array_size;
 								break;
-#ifndef _WIN64
+#if ! ARCH_64
 							} else if (elem_desc==(Int)&REAL+2){
 								array_size<<=1;
 							
@@ -212,7 +222,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 								break;
 #endif
 							} else if (elem_desc==(Int)&BOOL+2){
-#ifdef _WIN64
+#if ARCH_64
 								array_size=(array_size+7)>>3;
 #else
 								array_size=(array_size+3)>>2;
@@ -316,7 +326,7 @@ Int *copy_string_to_graph (Int *string_p,void *begin_free_heap,void *end_free_he
 #ifdef OLD_DESCRIPTORS
 							desc-=10;
 #else
-# ifdef _WIN64
+# if ARCH_64
 							desc-=10;
 # else
 							desc-=6;
@@ -680,12 +690,12 @@ void remove_forwarding_pointers_from_string (Int *string_p,Int *end_forwarding_p
 				arity=((unsigned short *)desc)[-1];
 				if (arity==0){
 					if (desc==(Int)&INT_descriptor+2 || desc==(Int)&CHAR+2 || desc==(Int)&BOOL+2
-#ifdef _WIN64
+#if ARCH_64
 						|| desc==(Int)&REAL+2
 #endif
 					){
 						string_p+=2;
-#ifndef _WIN64
+#if ! ARCH_64
 					} else if (desc==(Int)&REAL+2){
 						string_p+=3;
 #endif
@@ -694,7 +704,7 @@ void remove_forwarding_pointers_from_string (Int *string_p,Int *end_forwarding_p
 							
 						length=string_p[1];
 						string_p+=2;
-#ifdef _WIN64
+#if ARCH_64
 						n_words=(length+7)>>3;
 #else
 						n_words=(length+3)>>2;
@@ -709,18 +719,18 @@ void remove_forwarding_pointers_from_string (Int *string_p,Int *end_forwarding_p
 													
 						if (elem_desc==0){
 						} else if (elem_desc==(Int)&INT_descriptor+2
-#ifdef _WIN64
+#if ARCH_64
 							|| elem_desc==(Int)&REAL+2
 #endif
 						){
 							string_p+=array_size;
-#ifndef _WIN64
+#if ! ARCH_64
 						} else if (elem_desc==(Int)&REAL+2){
 							array_size<<=1;
 							string_p+=array_size;
 #endif
 						} else if (elem_desc==(Int)&BOOL+2){
-#ifdef _WIN64
+#if ARCH_64
 							array_size=(array_size+7)>>3;
 #else
 							array_size=(array_size+3)>>2;
