ae57a5e002b01c364331c2c7c94941a170a55f67
Jurriën Stutterheim <j.stutterheim@cs.ru.nl>
2013-02-15 13:34:12 +0000

    Add automatically generated stuff to bootstrap.js
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@2338 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/bootstrap.js
+++ Client/bootstrap.js
@@ -1,4 +1,355 @@
 Ext.Loader.addClassPathMappings({
+  "itwc.patch.grid.GridViewLayout": "app/patch/grid/View.js",
   "Ext": "ext/src",
-  "itwc": "app"
+  "itwc.layout.component": "app/component",
+  "itwc": "app",
+  "Ext.ux.GroupTreeChunker": "ext/src/ux/GroupTabPanel.js"
+});Ext.ClassManager.addNameAlternateMappings({
+  "itwc.component.edit.Int": [],
+  "Ext.ux.grid.TransformGrid": [],
+  "Ext.ux.ajax.DataSimlet": [],
+  "itwc.controller.Controller": [],
+  "itwc.component.view.String": [],
+  "Ext.ux.event.Driver": [],
+  "Ext.ux.ajax.XmlSimlet": [],
+  "itwc.component.edit.Date": [],
+  "itwc.component.misc.Icon": [],
+  "Ext.ux.DataView.DragSelector": [],
+  "itwc.patch.grid.View": [],
+  "Ext.ux.grid.menu.ListMenu": [],
+  "itwc.container.Container": [],
+  "Ext.ux.form.MultiSelect": [
+    "Ext.ux.Multiselect"
+  ],
+  "itwc.component.edit.Editable": [],
+  "itwc.component.view.Document": [],
+  "Ext.ux.CheckColumn": [],
+  "Ext.ux.PreviewPlugin": [],
+  "itwc.layout.component.edit.Code": [],
+  "itwc.view.Main": [],
+  "itwc.component.action.MenuButton": [],
+  "Ext.ux.ajax.SimXhr": [],
+  "itwc.component.action.ActionButton": [],
+  "Ext.ux.event.RecorderManager": [],
+  "Ext.ux.ProgressBarPager": [],
+  "Ext.ux.ajax.Simlet": [],
+  "itwc.container.Window": [],
+  "Ext.ux.SlidingPager": [],
+  "itwc.controller.Main": [],
+  "Ext.ux.TabReorderer": [],
+  "Ext.ux.grid.filter.NumericFilter": [],
+  "Ext.ux.event.Recorder": [],
+  "itwc.container.Tasklet": [],
+  "itwc.patch.grid.GridViewLayout": [],
+  "Ext.ux.ajax.SimManager": [],
+  "itwc.container.Panel": [],
+  "itwc.component.view.Progress": [],
+  "Ext.ux.grid.filter.BooleanFilter": [],
+  "itwc.component.choice.CheckboxGroup": [],
+  "Ext.ux.TabScrollerMenu": [],
+  "Ext.ux.IFrame": [],
+  "Ext.ux.form.SearchField": [],
+  "Ext.ux.LiveSearchGridPanel": [],
+  "Ext.ux.grid.filter.DateFilter": [],
+  "Ext.ux.TreePicker": [],
+  "itwc.component.edit.Document": [],
+  "itwc.component.misc.Splitter": [],
+  "itwc.component.edit.Time": [],
+  "Ext.ux.data.PagingMemoryProxy": [
+    "Ext.data.PagingMemoryProxy"
+  ],
+  "itwc.component.edit.Code": [],
+  "Ext.ux.BoxReorderer": [],
+  "Ext.ux.grid.filter.Filter": [],
+  "Ext.ux.FieldReplicator": [],
+  "Ext.ux.grid.filter.ListFilter": [],
+  "itwc.container.Viewport": [],
+  "itwc.component.view.Slider": [],
+  "itwc.component.misc.Label": [],
+  "itwc.component.edit.Password": [],
+  "itwc.component.edit.Decimal": [],
+  "Ext.ux.GMapPanel": [],
+  "Ext.ux.GroupTabPanel": [],
+  "Ext.ux.DataView.LabelEditor": [],
+  "Ext.ux.grid.filter.StringFilter": [],
+  "Ext.ux.GroupTreeChunker": [],
+  "Ext.ux.statusbar.ValidationStatus": [],
+  "Ext.ux.layout.Center": [],
+  "itwc.component.choice.Grid": [],
+  "itwc.component.action.ActionMenuItem": [],
+  "Ext.ux.DataView.Animated": [],
+  "itwc.component.view.Checkbox": [],
+  "itwc.component.choice.Tree": [],
+  "itwc.view.Viewport": [],
+  "Ext.ux.statusbar.StatusBar": [
+    "Ext.ux.StatusBar"
+  ],
+  "itwc.component.edit.Slider": [],
+  "itwc.component.edit.EditButton": [],
+  "Ext.ux.grid.menu.RangeMenu": [],
+  "Ext.ux.RowExpander": [],
+  "Ext.ux.event.Player": [],
+  "itwc.container.FieldSet": [],
+  "itwc.component.edit.GoogleMap": [],
+  "itwc.component.action.SubMenuItem": [],
+  "Ext.ux.ToolbarDroppable": [],
+  "itwc.container.HotkeyArea": [],
+  "Ext.ux.event.Maker": [],
+  "itwc.component.action.Menu": [],
+  "itwc.container.TaskletPlaceholder": [],
+  "itwc.component.choice.Dropdown": [],
+  "itwc.component.edit.Note": [],
+  "Ext.ux.DataView.Draggable": [],
+  "itwc.component.choice.RadioGroup": [],
+  "Ext.ux.TabCloseMenu": [],
+  "itwc.layout.container.Box": [],
+  "itwc.component.view.HTML": [],
+  "Ext.ux.form.ItemSelector": [
+    "Ext.ux.ItemSelector"
+  ],
+  "itwc.component.misc.Tab": [],
+  "Ext.ux.ajax.JsonSimlet": [],
+  "Ext.ux.Spotlight": [],
+  "Ext.ux.grid.FiltersFeature": [],
+  "itwc.component.edit.String": [],
+  "itwc.patch.layout.Context": [],
+  "itwc.component.edit.Checkbox": []
+});Ext.ClassManager.addNameAliasMappings({
+  "itwc.component.edit.Int": [
+    "widget.itwc_edit_int"
+  ],
+  "Ext.ux.grid.TransformGrid": [],
+  "Ext.ux.ajax.DataSimlet": [],
+  "itwc.controller.Controller": [],
+  "itwc.component.view.String": [
+    "widget.itwc_view_string"
+  ],
+  "Ext.ux.event.Driver": [],
+  "Ext.ux.ajax.XmlSimlet": [
+    "simlet.xml"
+  ],
+  "itwc.component.edit.Date": [
+    "widget.itwc_edit_date"
+  ],
+  "itwc.component.misc.Icon": [
+    "widget.itwc_icon"
+  ],
+  "Ext.ux.DataView.DragSelector": [],
+  "itwc.patch.grid.View": [],
+  "Ext.ux.grid.menu.ListMenu": [],
+  "itwc.container.Container": [
+    "widget.itwc_container"
+  ],
+  "Ext.ux.form.MultiSelect": [
+    "widget.multiselectfield",
+    "widget.multiselect"
+  ],
+  "itwc.component.edit.Editable": [],
+  "itwc.component.view.Document": [
+    "widget.itwc_view_document"
+  ],
+  "Ext.ux.CheckColumn": [
+    "widget.checkcolumn"
+  ],
+  "Ext.ux.PreviewPlugin": [
+    "plugin.preview"
+  ],
+  "itwc.layout.component.edit.Code": [
+    "layout.itwc_edit_code"
+  ],
+  "itwc.view.Main": [],
+  "itwc.component.action.MenuButton": [
+    "widget.itwc_menubutton"
+  ],
+  "Ext.ux.ajax.SimXhr": [],
+  "itwc.component.action.ActionButton": [
+    "widget.itwc_actionbutton"
+  ],
+  "Ext.ux.event.RecorderManager": [
+    "widget.eventrecordermanager"
+  ],
+  "Ext.ux.ProgressBarPager": [],
+  "Ext.ux.ajax.Simlet": [
+    "simlet.basic"
+  ],
+  "itwc.container.Window": [
+    "widget.itwc_window"
+  ],
+  "Ext.ux.SlidingPager": [],
+  "itwc.controller.Main": [],
+  "Ext.ux.TabReorderer": [],
+  "Ext.ux.grid.filter.NumericFilter": [
+    "gridfilter.numeric"
+  ],
+  "Ext.ux.event.Recorder": [],
+  "itwc.container.Tasklet": [
+    "widget.itwc_tasklet"
+  ],
+  "itwc.patch.grid.GridViewLayout": [
+    "layout.gridviewlayout"
+  ],
+  "Ext.ux.ajax.SimManager": [],
+  "itwc.container.Panel": [
+    "widget.itwc_panel"
+  ],
+  "itwc.component.view.Progress": [
+    "widget.itwc_view_progress"
+  ],
+  "Ext.ux.grid.filter.BooleanFilter": [
+    "gridfilter.boolean"
+  ],
+  "itwc.component.choice.CheckboxGroup": [
+    "widget.itwc_choice_checkboxgroup"
+  ],
+  "Ext.ux.TabScrollerMenu": [
+    "plugin.tabscrollermenu"
+  ],
+  "Ext.ux.IFrame": [
+    "widget.uxiframe"
+  ],
+  "Ext.ux.form.SearchField": [
+    "widget.searchfield"
+  ],
+  "Ext.ux.LiveSearchGridPanel": [],
+  "Ext.ux.grid.filter.DateFilter": [
+    "gridfilter.date"
+  ],
+  "Ext.ux.TreePicker": [
+    "widget.treepicker"
+  ],
+  "itwc.component.edit.Document": [
+    "widget.itwc_edit_document"
+  ],
+  "itwc.component.misc.Splitter": [
+    "widget.itwc_splitter"
+  ],
+  "itwc.component.edit.Time": [
+    "widget.itwc_edit_time"
+  ],
+  "Ext.ux.data.PagingMemoryProxy": [
+    "proxy.pagingmemory"
+  ],
+  "itwc.component.edit.Code": [
+    "widget.itwc_edit_code"
+  ],
+  "Ext.ux.BoxReorderer": [],
+  "Ext.ux.grid.filter.Filter": [],
+  "Ext.ux.FieldReplicator": [],
+  "Ext.ux.grid.filter.ListFilter": [
+    "gridfilter.list"
+  ],
+  "itwc.container.Viewport": [],
+  "itwc.component.view.Slider": [
+    "widget.itwc_view_slider"
+  ],
+  "itwc.component.misc.Label": [
+    "widget.itwc_label"
+  ],
+  "itwc.component.edit.Password": [
+    "widget.itwc_edit_password"
+  ],
+  "itwc.component.edit.Decimal": [
+    "widget.itwc_edit_decimal"
+  ],
+  "Ext.ux.GMapPanel": [
+    "widget.gmappanel"
+  ],
+  "Ext.ux.GroupTabPanel": [
+    "widget.grouptabpanel"
+  ],
+  "Ext.ux.DataView.LabelEditor": [],
+  "Ext.ux.grid.filter.StringFilter": [
+    "gridfilter.string"
+  ],
+  "Ext.ux.GroupTreeChunker": [],
+  "Ext.ux.statusbar.ValidationStatus": [],
+  "Ext.ux.layout.Center": [
+    "layout.ux.center"
+  ],
+  "itwc.component.choice.Grid": [
+    "widget.itwc_choice_grid"
+  ],
+  "itwc.component.action.ActionMenuItem": [
+    "widget.itwc_actionmenuitem"
+  ],
+  "Ext.ux.DataView.Animated": [],
+  "itwc.component.view.Checkbox": [
+    "widget.itwc_view_checkbox"
+  ],
+  "itwc.component.choice.Tree": [
+    "widget.itwc_choice_tree"
+  ],
+  "itwc.view.Viewport": [],
+  "Ext.ux.statusbar.StatusBar": [
+    "widget.statusbar"
+  ],
+  "itwc.component.edit.Slider": [
+    "widget.itwc_edit_slider"
+  ],
+  "itwc.component.edit.EditButton": [
+    "widget.itwc_editbutton"
+  ],
+  "Ext.ux.grid.menu.RangeMenu": [],
+  "Ext.ux.RowExpander": [
+    "plugin.rowexpander"
+  ],
+  "Ext.ux.event.Player": [],
+  "itwc.container.FieldSet": [
+    "widget.itwc_fieldset"
+  ],
+  "itwc.component.edit.GoogleMap": [
+    "widget.itwc_edit_googlemap"
+  ],
+  "itwc.component.action.SubMenuItem": [
+    "widget.itwc_submenuitem"
+  ],
+  "Ext.ux.ToolbarDroppable": [],
+  "itwc.container.HotkeyArea": [],
+  "Ext.ux.event.Maker": [],
+  "itwc.component.action.Menu": [
+    "widget.itwc_menu"
+  ],
+  "itwc.container.TaskletPlaceholder": [
+    "widget.itwc_tasklet_placeholder"
+  ],
+  "itwc.component.choice.Dropdown": [
+    "widget.itwc_choice_dropdown"
+  ],
+  "itwc.component.edit.Note": [
+    "widget.itwc_edit_note"
+  ],
+  "Ext.ux.DataView.Draggable": [],
+  "itwc.component.choice.RadioGroup": [
+    "widget.itwc_choice_radiogroup"
+  ],
+  "Ext.ux.TabCloseMenu": [
+    "plugin.tabclosemenu"
+  ],
+  "itwc.layout.container.Box": [
+    "layout.itwc_box"
+  ],
+  "itwc.component.view.HTML": [
+    "widget.itwc_view_html"
+  ],
+  "Ext.ux.form.ItemSelector": [
+    "widget.itemselectorfield",
+    "widget.itemselector"
+  ],
+  "itwc.component.misc.Tab": [
+    "widget.itwc_tab"
+  ],
+  "Ext.ux.ajax.JsonSimlet": [
+    "simlet.json"
+  ],
+  "Ext.ux.Spotlight": [],
+  "Ext.ux.grid.FiltersFeature": [
+    "feature.filters"
+  ],
+  "itwc.component.edit.String": [
+    "widget.itwc_edit_string"
+  ],
+  "itwc.patch.layout.Context": [],
+  "itwc.component.edit.Checkbox": [
+    "widget.itwc_edit_checkbox"
+  ]
 });
