eaa9b5bd4288ddfc09822c29dba2a500715aee94
Jurriën Stutterheim <j.stutterheim@cs.ru.nl>
2013-10-11 08:12:24 +0000

    Expand stencil set for tonic
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/trunk@2720 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Server/iTasks/API/Extensions/Tonic/WebPublic/joint/plugins/joint.shapes.tonic.js
+++ Server/iTasks/API/Extensions/Tonic/WebPublic/joint/plugins/joint.shapes.tonic.js
@@ -1,145 +1,145 @@
 joint.shapes.tonic = {};
 
-joint.shapes.tonic.Class = joint.shapes.basic.Generic.extend({
+//joint.shapes.tonic.Class = joint.shapes.basic.Generic.extend({
 
-    markup: [
-        '<g class="rotatable">',
-          '<g class="scalable">',
-            '<rect class="tonic-class-name-rect"/><rect class="tonic-class-attrs-rect"/><rect class="tonic-class-methods-rect"/>',
-          '</g>',
-          '<text class="tonic-class-name-text"/><text class="tonic-class-attrs-text"/><text class="tonic-class-methods-text"/>',
-        '</g>'
-    ].join(''),
+    //markup: [
+        //'<g class="rotatable">',
+          //'<g class="scalable">',
+            //'<rect class="tonic-class-name-rect"/><rect class="tonic-class-attrs-rect"/><rect class="tonic-class-methods-rect"/>',
+          //'</g>',
+          //'<text class="tonic-class-name-text"/><text class="tonic-class-attrs-text"/><text class="tonic-class-methods-text"/>',
+        //'</g>'
+    //].join(''),
 
-    defaults: joint.util.deepSupplement({
+    //defaults: joint.util.deepSupplement({
 
-        type: 'tonic.Class',
+        //type: 'tonic.Class',
 
-        attrs: {
-            rect: { 'width': 200, 'stroke': 'black', 'stroke-width': 2, 'fill': '#2980b9' },
-            text: { 'fill': 'black', 'font-size': 12, 'font-family': 'Times New Roman' },
+        //attrs: {
+            //rect: { 'width': 200, 'stroke': 'black', 'stroke-width': 2, 'fill': '#2980b9' },
+            //text: { 'fill': 'black', 'font-size': 12, 'font-family': 'Times New Roman' },
 
-            '.tonic-class-name-rect': { 'fill': '#3498db' },
-            '.tonic-class-attrs-rect': {},
-            '.tonic-class-methods-rect': {},
+            //'.tonic-class-name-rect': { 'fill': '#3498db' },
+            //'.tonic-class-attrs-rect': {},
+            //'.tonic-class-methods-rect': {},
 
-            '.tonic-class-name-text': { 'ref': '.tonic-class-name-rect', 'ref-y': .5, 'ref-x': .5, 'text-anchor': 'middle', 'y-alignment': 'middle', 'font-weight': 'bold' },
-            '.tonic-class-attrs-text': { 'ref': '.tonic-class-attrs-rect', 'ref-y': 5, 'ref-x': 5 },
-            '.tonic-class-methods-text': { 'ref': '.tonic-class-methods-rect', 'ref-y': 5, 'ref-x': 5 }
+            //'.tonic-class-name-text': { 'ref': '.tonic-class-name-rect', 'ref-y': .5, 'ref-x': .5, 'text-anchor': 'middle', 'y-alignment': 'middle', 'font-weight': 'bold' },
+            //'.tonic-class-attrs-text': { 'ref': '.tonic-class-attrs-rect', 'ref-y': 5, 'ref-x': 5 },
+            //'.tonic-class-methods-text': { 'ref': '.tonic-class-methods-rect', 'ref-y': 5, 'ref-x': 5 }
 
-        },
+        //},
 
-        name: [],
-        attributes: [],
-        methods: []
+        //name: [],
+        //attributes: [],
+        //methods: []
 
-    }, joint.shapes.basic.Generic.prototype.defaults),
+    //}, joint.shapes.basic.Generic.prototype.defaults),
 
-    initialize: function() {
+    //initialize: function() {
 
-        _.bindAll(this, 'updateRectangles');
+        //_.bindAll(this, 'updateRectangles');
 
-        this.on('change:name change:attributes change:methods', function() {
-            this.updateRectangles();
-            this.trigger('change:attrs');
-        });
+        //this.on('change:name change:attributes change:methods', function() {
+            //this.updateRectangles();
+            //this.trigger('change:attrs');
+        //});
 
-        this.updateRectangles();
+        //this.updateRectangles();
 
-        joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
-    },
+        //joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
+    //},
 
-    getClassName: function() {
-        return this.get('name');
-    },
+    //getClassName: function() {
+        //return this.get('name');
+    //},
 
-    updateRectangles: function() {
+    //updateRectangles: function() {
 
-        var attrs = this.get('attrs');
+        //var attrs = this.get('attrs');
 
-        var rects = [
-            { type: 'name', text: this.getClassName() },
-            { type: 'attrs', text: this.get('attributes') },
-            { type: 'methods', text: this.get('methods') }
-        ];
+        //var rects = [
+            //{ type: 'name', text: this.getClassName() },
+            //{ type: 'attrs', text: this.get('attributes') },
+            //{ type: 'methods', text: this.get('methods') }
+        //];
 
-        var offsetY = 0;
+        //var offsetY = 0;
 
-        _.each(rects, function(rect) {
+        //_.each(rects, function(rect) {
 
-            var lines = _.isArray(rect.text) ? rect.text : [rect.text];
-	    var rectHeight = lines.length * 20 + 20;
+            //var lines = _.isArray(rect.text) ? rect.text : [rect.text];
+		//var rectHeight = lines.length * 20 + 20;
 
-            attrs['.tonic-class-' + rect.type + '-text'].text = lines.join('\n');
-            attrs['.tonic-class-' + rect.type + '-rect'].height = rectHeight;
-            attrs['.tonic-class-' + rect.type + '-rect'].transform = 'translate(0,'+ offsetY + ')';
+            //attrs['.tonic-class-' + rect.type + '-text'].text = lines.join('\n');
+            //attrs['.tonic-class-' + rect.type + '-rect'].height = rectHeight;
+            //attrs['.tonic-class-' + rect.type + '-rect'].transform = 'translate(0,'+ offsetY + ')';
 
-            offsetY += rectHeight;
-        });
-    }
+            //offsetY += rectHeight;
+        //});
+    //}
 
-});
+//});
 
-joint.shapes.tonic.Abstract = joint.shapes.tonic.Class.extend({
+//joint.shapes.tonic.Abstract = joint.shapes.tonic.Class.extend({
 
-    defaults: joint.util.deepSupplement({
-        type: 'tonic.Abstract',
-        attrs: { 'rect' : { fill : '#c0392b' }, '.tonic-class-name-rect': { fill : '#e74c3c' }}
-    }, joint.shapes.tonic.Class.prototype.defaults),
+    //defaults: joint.util.deepSupplement({
+        //type: 'tonic.Abstract',
+        //attrs: { 'rect' : { fill : '#c0392b' }, '.tonic-class-name-rect': { fill : '#e74c3c' }}
+    //}, joint.shapes.tonic.Class.prototype.defaults),
 
-    getClassName: function() {
-        return ['<<Abstract>>', this.get('name')];
-    }
+    //getClassName: function() {
+        //return ['<<Abstract>>', this.get('name')];
+    //}
 
-});
+//});
 
-joint.shapes.tonic.Interface = joint.shapes.tonic.Class.extend({
+//joint.shapes.tonic.Interface = joint.shapes.tonic.Class.extend({
 
-    defaults: joint.util.deepSupplement({
-        type: 'tonic.Interface',
-        attrs: { 'rect' : { fill : '#f39c12' }, '.tonic-class-name-rect': { fill : '#f1c40f' }}
-    }, joint.shapes.tonic.Class.prototype.defaults),
+    //defaults: joint.util.deepSupplement({
+        //type: 'tonic.Interface',
+        //attrs: { 'rect' : { fill : '#f39c12' }, '.tonic-class-name-rect': { fill : '#f1c40f' }}
+    //}, joint.shapes.tonic.Class.prototype.defaults),
 
-    getClassName: function() {
-        return ['<<Interface>>', this.get('name')];
-    }
+    //getClassName: function() {
+        //return ['<<Interface>>', this.get('name')];
+    //}
 
-});
+//});
 
-joint.shapes.tonic.Generalization = joint.dia.Link.extend({
-    defaults: {
-        type: 'tonic.Generalization',
-        attrs: { '.marker-target': { d: 'M 20 0 L 0 10 L 20 20 z', fill: 'white' }}
-    }
-});
+//joint.shapes.tonic.Generalization = joint.dia.Link.extend({
+    //defaults: {
+        //type: 'tonic.Generalization',
+        //attrs: { '.marker-target': { d: 'M 20 0 L 0 10 L 20 20 z', fill: 'white' }}
+    //}
+//});
 
-joint.shapes.tonic.Implementation = joint.dia.Link.extend({
-    defaults: {
-        type: 'tonic.Implementation',
-        attrs: {
-            '.marker-target': { d: 'M 20 0 L 0 10 L 20 20 z', fill: 'white' },
-            '.connection': { 'stroke-dasharray': '3,3' }
-        }
-    }
-});
+//joint.shapes.tonic.Implementation = joint.dia.Link.extend({
+    //defaults: {
+        //type: 'tonic.Implementation',
+        //attrs: {
+            //'.marker-target': { d: 'M 20 0 L 0 10 L 20 20 z', fill: 'white' },
+            //'.connection': { 'stroke-dasharray': '3,3' }
+        //}
+    //}
+//});
 
-joint.shapes.tonic.Aggregation = joint.dia.Link.extend({
-    defaults: {
-        type: 'tonic.Aggregation',
-        attrs: { '.marker-target': { d: 'M 40 10 L 20 20 L 0 10 L 20 0 z', fill: 'white' }}
-    }
-});
+//joint.shapes.tonic.Aggregation = joint.dia.Link.extend({
+    //defaults: {
+        //type: 'tonic.Aggregation',
+        //attrs: { '.marker-target': { d: 'M 40 10 L 20 20 L 0 10 L 20 0 z', fill: 'white' }}
+    //}
+//});
 
-joint.shapes.tonic.Composition = joint.dia.Link.extend({
-    defaults: {
-        type: 'tonic.Composition',
-        attrs: { '.marker-target': { d: 'M 40 10 L 20 20 L 0 10 L 20 0 z', fill: 'black' }}
-    }
-});
+//joint.shapes.tonic.Composition = joint.dia.Link.extend({
+    //defaults: {
+        //type: 'tonic.Composition',
+        //attrs: { '.marker-target': { d: 'M 40 10 L 20 20 L 0 10 L 20 0 z', fill: 'black' }}
+    //}
+//});
 
-joint.shapes.tonic.Association = joint.dia.Link.extend({
-    defaults: { type: 'tonic.Association' }
-});
+//joint.shapes.tonic.Association = joint.dia.Link.extend({
+    //defaults: { type: 'tonic.Association' }
+//});
 
 joint.shapes.tonic.TaskApp = joint.shapes.basic.Generic.extend({
 
@@ -160,7 +160,7 @@ joint.shapes.tonic.TaskApp = joint.shapes.basic.Generic.extend({
             rect: { 'width': 200, 'height': 200, 'fill': '#ffffff', 'stroke': '#000000', 'stroke-width': 3, 'rx': 10, 'ry': 10 },
             path: { 'd': 'M 0 20 L 200 20', 'stroke': '#000000', 'stroke-width': 2 },
             text: { 'font-family': 'Tahoma', 'font-size': 14, fill: 'black' },
-            '.tonic-taskApp-name': { 'ref': 'rect', 'ref-x': .5, 'ref-y': 5, 'text-anchor': 'middle', 'font-weight': 'bold'},
+            '.tonic-taskApp-name': { 'ref': 'rect', 'ref-x': 0.5, 'ref-y': 5, 'text-anchor': 'middle', 'font-weight': 'bold'},
             '.tonic-taskApp-events': { 'ref': 'path', 'ref-x': 5, 'ref-y': 5 }
         },
 
@@ -195,7 +195,7 @@ joint.shapes.tonic.TaskApp = joint.shapes.basic.Generic.extend({
     },
 
     updatePath: function() {
-        this.get('attrs')['path'].d = 'M 0 20 L ' + this.get('size').width + ' 20';
+        this.get('attrs').path.d = 'M 0 20 L ' + this.get('size').width + ' 20';
     }
 
 });
@@ -231,7 +231,33 @@ joint.shapes.tonic.StopState = joint.shapes.basic.Rect.extend({
         type: 'tonic.StopState',
         size: { width: 20, height: 20 },
         attrs: {
-          rect: { 'width': 200, 'height': 200, 'fill': '#000000', 'stroke': '#000000', 'stroke-width': 0, 'rx': 10, 'ry': 10 },
+          rect: { 'width': 200, 'height': 200, 'fill': '#000000', 'stroke': '#000000', 'stroke-width': 0, 'rx': 10, 'ry': 10 }
+        }
+
+    }, joint.shapes.basic.Rect.prototype.defaults)
+});
+
+joint.shapes.tonic.DecisionState = joint.shapes.basic.Rect.extend({
+    defaults: joint.util.deepSupplement({
+
+        type: 'tonic.DecisionState',
+        size: { width: 100, height: 100 },
+        attrs: {
+          rect: { 'width': 200, 'height': 200, 'fill': '#ffffff', 'stroke': '#000000',
+                  'stroke-width': 2, 'rx': 10, 'ry': 10, 'transform': 'rotate(-45 100 100)' }
+        }
+
+    }, joint.shapes.basic.Rect.prototype.defaults)
+});
+
+joint.shapes.tonic.LetState = joint.shapes.basic.Rect.extend({
+    defaults: joint.util.deepSupplement({
+
+        type: 'tonic.LetState',
+        size: { width: 250, height: 50 },
+        attrs: {
+          rect: { 'width': 250, 'height': 50, 'fill': '#ffffff', 'stroke': '#000000',
+                  'stroke-width': 2, 'rx': 10, 'ry': 10 }
         }
 
     }, joint.shapes.basic.Rect.prototype.defaults)
