e1707966b0175433f0d006fb1b3ca43756e67613
Jurriën Stutterheim <j.stutterheim@cs.ru.nl>
2013-11-11 12:51:51 +0000

    Two more strict comparisons
    git-svn-id: https://svn.cs.ru.nl/repos/iTask-system/branches/wessel@2780 63da3aa8-80fd-4f01-9db8-e6ea747a3da2

--- Client/app/taskeval/sapl-rt.js
+++ Client/app/taskeval/sapl-rt.js
@@ -1,8 +1,8 @@
-_feval = function (expr) { // check on length split for == and <=
+_feval = function (expr) { // check on length split for === and <=
 		var y, f, xs;
 		while (1) {
 			if (typeof (expr) === "object") {
-				if (expr.length == 1) return expr[0]; // boxed primitive
+				if (expr.length === 1) return expr[0]; // boxed primitive
 				else if (typeof (expr[0]) === "function") { // closure
 					f = expr[0];
 					xs = expr[1];
@@ -21,8 +21,8 @@ _feval = function (expr) { // check on length split for == and <=
 					expr[0] = y[0];
 					expr[1] = y[1].concat(expr[1]);
 				} else return expr; // constructor
-			} else if (typeof (expr) == "function"){
-				if (expr.length == 0) return expr.apply(this, []);
+			} else if (typeof (expr) === "function"){
+				if (expr.length === 0) return expr.apply(this, []);
 				expr = [expr, []];
 			}
 			else // simple value
@@ -42,7 +42,7 @@ _fapp = function (f,xs){
 				f = f.apply(this, xs.splice(0, f.length));
 			} else // not enough args: convert it thunk
 				return [f,xs];
-		// typeof f == "object"
+		// typeof f === "object"
 		} else { // curried application -> uncurry
 			xs = f[1].concat(xs);
 			f = f[0];			
