460668645d106922d2c9c9ccf53773e76c725a3f
Mart Lubbers <mart@martlubbers.net>
2018-06-11 15:02:21 +0200

    Add compilations for all programs to the ci-tests

---
+++ Tests/ci-tests.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+CLMFLAGS="-dynamics -h 200m -s 10m"
+CLMLIBS="-IL Dynamics -IL GraphCopy -IL Sapl -IL TCPIP -IL Platform -IL Platform/Deprecated/StdLib -I $(pwd)/Libraries"
+CLM="clm $CLMFLAGS $CLMLIBS"
+
+( cd Examples; $CLM BasicAPIExamples; )
+( cd Examples/Games/Ligretto; $CLM -I .. -I ../../Graphics Ligretto; )
+( cd Examples/Games/Trax; $CLM -I .. Trax; )
+( cd Examples/GIS; $CLM LeafletMapExample; )
+( cd Examples/Graphics/BasicImagesExamples; for i in *.prj.default; do $CLM -I .. $(basename -s .prj.default $i); done; )
+( cd Examples/Applications/TheTaxMan; $CLM TheTaxMan; )
+#( cd Examples/Applications/Incidone; $CLM IncidoneCCC; )
+#( cd Examples/Applications/ShipAdventure; $CLM main; )
