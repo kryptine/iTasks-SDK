4d30b933b255b117f6fdf94b685595423b494342
Mart Lubbers <mart@martlubbers.net>
2018-06-12 08:45:13 +0200

    Add default project files for all examples and update ci-test.sh to use cpm

--- Tests/ci-tests.bash
+++ Tests/ci-tests.bash
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
-
-CLMFLAGS="-dynamics -h 200m -s 10m"
-CLMLIBS="-IL Dynamics -IL GraphCopy -IL Sapl -IL TCPIP -IL Platform -IL Platform/Deprecated/StdLib -I $(pwd)/Libraries"
-CLM="clm $CLMFLAGS $CLMLIBS"
-
-( cd Examples; $CLM BasicAPIExamples; )
-( cd Examples/Games/Ligretto; $CLM -I .. -I ../../Graphics Ligretto; )
-( cd Examples/Games/Trax; $CLM -I .. Trax; )
-( cd Examples/GIS; $CLM LeafletMapExample; )
-( cd Examples/Graphics/BasicImagesExamples; for i in *.prj.default; do $CLM -I .. $(basename -s .prj.default $i); done; )
-( cd Examples/Applications/TheTaxMan; $CLM TheTaxMan; )
-#( cd Examples/Applications/Incidone; $CLM IncidoneCCC; )
-#( cd Examples/Applications/ShipAdventure; $CLM main; )
+trap 'mv -v /opt/clean/etc/IDEEnvs{.bak,}' EXIT
+cp -v /opt/clean/etc/IDEEnvs{,.bak}
+sed -i "s|{Application}/lib/iTasks|$(pwd)/Libraries|g" /opt/clean/etc/IDEEnvs
+find . -name "*.prj" | xargs dirname | sort -u | xargs -I{} sh -c "cd {}; cpm make"
