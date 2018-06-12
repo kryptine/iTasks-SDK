f774eabac532ac5c1aadb8fe0c571796a09d4e68
Mart Lubbers <mart@martlubbers.net>
2018-06-12 09:04:43 +0200

    Fix test script to be able to work with prj.default

--- Tests/ci-tests.bash
+++ Tests/ci-tests.bash
@@ -5,7 +5,10 @@ cp -v /opt/clean/etc/IDEEnvs{,.bak}
 sed -i "s|{Application}/lib/iTasks|$(pwd)/Libraries|g" /opt/clean/etc/IDEEnvs
 
 #Try to compile everything
+find . -name "*.prj.default" | while read f; do
+		mv "$f" "$(dirname $f)/$(basename -s .prj.default $f)".prj
+	done
 find . -name "*.prj" | xargs dirname | sort -u | xargs -I{} sh -c "cd {}; cpm make"
 
 #Run the tests
-( cd Tools; ./RunUnitTestsForCi; )
+( cd Tools; ./RunUnitTestsForCI; )
