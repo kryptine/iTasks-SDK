191af8291237f58633a9aef139dec4623cdd5cf9
Mart Lubbers <mart@martlubbers.net>
2018-06-12 08:48:31 +0200

    also run the ci tests

--- Tests/ci-tests.bash
+++ Tests/ci-tests.bash
@@ -3,4 +3,9 @@ set -e
 trap 'mv -v /opt/clean/etc/IDEEnvs{.bak,}' EXIT
 cp -v /opt/clean/etc/IDEEnvs{,.bak}
 sed -i "s|{Application}/lib/iTasks|$(pwd)/Libraries|g" /opt/clean/etc/IDEEnvs
+
+#Try to compile everything
 find . -name "*.prj" | xargs dirname | sort -u | xargs -I{} sh -c "cd {}; cpm make"
+
+#Run the tests
+( cd Tools; ./RunUnitTestsForCi; )
