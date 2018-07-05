5927a37eb9361353a65d6a9a3bd9a421015ab219
Mart Lubbers <mart@martlubbers.net>
2018-07-05 10:29:55 +0200

    Update ci-test
    - work with $CLEAN_HOME
    - use xargs for better failure detection

--- ci-tests.bash
+++ ci-tests.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-if [ -e /opt/clean/etc/IDEEnvs ]; then
-	trap 'mv -v /opt/clean/etc/IDEEnvs{.bak,}' EXIT
-	cp -v /opt/clean/etc/IDEEnvs{,.bak}
-	sed -i "s|{Application}/lib/iTasks|$(pwd)/Libraries|g" /opt/clean/etc/IDEEnvs
-	sed -i 's#EnvironmentLinker:	lib/exe/linker#&:-lmysqlclient -lsqlite3#g' /opt/clean/etc/IDEEnvs
+
+if [ -e "$CLEAN_HOME"/etc/IDEEnvs ]; then
+	trap 'mv -v "$CLEAN_HOME"/etc/IDEEnvs{.bak,}' EXIT
+	cp -v "$CLEAN_HOME"/etc/IDEEnvs{,.bak}
+	sed -i "s|{Application}/lib/iTasks|$(pwd)/Libraries|g" "$CLEAN_HOME"/etc/IDEEnvs
+	sed -i 's#EnvironmentLinker:	lib/exe/linker#&:-lmysqlclient -lsqlite3#g' "$CLEAN_HOME"/etc/IDEEnvs
 fi
 
 #Try to compile everything
@@ -14,4 +15,4 @@ find . -name "*.prj.default" | while read f; do
 find . -name "*.prj" -exec dirname {} \; | sort -u | xargs -I{} sh -c "cd {}; cpm make"
 
 #Run the unit tests
-find Tests/Unit -type f -perm 755 -exec cleantest -r {} \;
+find Tests/Unit -type f -perm 755 | xargs -n 1 cleantest -r
