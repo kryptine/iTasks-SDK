044d2675093f958f1800364edf9fc035f13d84c3
Bas Lijnse <bas@baslijnse.nl>
2019-09-09 13:12:51 +0200

    Replaced sass preprocessing by simple concatenation

--- Libraries/iTasks/UI/WebPublic/css/generate-css.sh
+++ Libraries/iTasks/UI/WebPublic/css/generate-css.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-sass --no-source-map src/main.scss itasks.css
-sass --no-source-map src/main.scss itasks-orangeblue.css
+cat src/options.css\
+	src/themes.css\
+	src/components-core.css\
+	src/components-raw.css\
+	src/components-container.css\
+	src/components-form.css\
+	src/components-display.css\
+	src/components-selection.css\
+	src/icons.css\
+	src/tooltip.css > itasks.css
