dfbfe0b283d2332049d0aeee8c4f7d9bf19ce87d
Mart Lubbers <mart@martlubbers.net>
2020-03-24 11:54:14 +0100

    add gast to environment (datetime requires it)

--- ci-tests.bash
+++ ci-tests.bash
@@ -26,6 +26,7 @@ if [ $(uname) = "Linux" ]; then
 	errors="$(
 		cd Examples
 		cp BasicAPIExamples.prj{.default,}
+		cpm project BasicAPIExamples.prj path add $CLEAN_HOME/lib/Gast >/dev/null 2>&1
 		find ../Libraries/ -name "*.dcl" -exec head -n 1 {} \; \
 			|  sed 's/definition module //g' \
 			|  xargs cpm project BasicAPIExamples.prj compile \
